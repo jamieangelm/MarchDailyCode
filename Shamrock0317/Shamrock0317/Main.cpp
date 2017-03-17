605884b9b6c898964b757ff90031fc72d79998a6
Game110AM <Game110AM@605-110-STU19>
2017-03-17 08:21:12 -0600

    commit

---
+++ Shamrock0317/Shamrock0317/Main.cpp
@@ -0,0 +1,22 @@
+//Using a for loop, populate a string array with user input.
+//Then, check if the 3rd slot contains the word "shamrock".
+//If it does, tell the user they get a pot of gold. If not, tell them to go away.
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+
+	string item[4];
+
+	cout << "give me four words" << endl;
+	for (int i = 0; i < 4; i++)
+		cin >> input[i];
+
+	if (item[2] == "shamrock")
+		cout << "you got the gold!" << endl;
+	else
+		cout << "you suck"
+	}
+}
